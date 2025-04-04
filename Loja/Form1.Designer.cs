99b709b64b077a69adfd48c71e6c14bc963ebe4c
cauagoncalves-p <cauagoncalves2190@gmail.com>
2025-04-04 13:48:18 -0300

    Adicionar arquivos de projeto.

---
+++ Loja/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace Loja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.lblBairro = new System.Windows.Forms.Label();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.lblCidade = new System.Windows.Forms.Label();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.txtCep = new System.Windows.Forms.TextBox();
+            this.lblCep = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.txtObservacao = new System.Windows.Forms.TextBox();
+            this.lblObservacao = new System.Windows.Forms.Label();
+            this.lblDataCadastro = new System.Windows.Forms.Label();
+            this.dtpDataCadastro = new System.Windows.Forms.DateTimePicker();
+            this.lboDados = new System.Windows.Forms.ListBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(24, 9);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(105, 20);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "Digite seu ID:";
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(24, 38);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(238, 26);
+            this.txtID.TabIndex = 1;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(24, 100);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(238, 26);
+            this.txtNome.TabIndex = 3;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(24, 76);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(130, 20);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Digite seu Nome:";
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEndereco.Location = new System.Drawing.Point(24, 173);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(238, 26);
+            this.txtEndereco.TabIndex = 5;
+            // 
+            // lblEndereco
+            // 
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEndereco.Location = new System.Drawing.Point(24, 141);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(155, 20);
+            this.lblEndereco.TabIndex = 4;
+            this.lblEndereco.Text = "Digite seu endereço:";
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBairro.Location = new System.Drawing.Point(24, 245);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(238, 26);
+            this.txtBairro.TabIndex = 7;
+            // 
+            // lblBairro
+            // 
+            this.lblBairro.AutoSize = true;
+            this.lblBairro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBairro.Location = new System.Drawing.Point(24, 213);
+            this.lblBairro.Name = "lblBairro";
+            this.lblBairro.Size = new System.Drawing.Size(128, 20);
+            this.lblBairro.TabIndex = 6;
+            this.lblBairro.Text = "Digite seu bairro:";
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCidade.Location = new System.Drawing.Point(24, 317);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(238, 26);
+            this.txtCidade.TabIndex = 9;
+            // 
+            // lblCidade
+            // 
+            this.lblCidade.AutoSize = true;
+            this.lblCidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCidade.Location = new System.Drawing.Point(24, 285);
+            this.lblCidade.Name = "lblCidade";
+            this.lblCidade.Size = new System.Drawing.Size(135, 20);
+            this.lblCidade.TabIndex = 8;
+            this.lblCidade.Text = "Digite sua cidade:";
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEstado.Location = new System.Drawing.Point(24, 391);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(238, 26);
+            this.txtEstado.TabIndex = 11;
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstado.Location = new System.Drawing.Point(24, 355);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(134, 20);
+            this.lblEstado.TabIndex = 10;
+            this.lblEstado.Text = "Digte seu estado:";
+            // 
+            // txtCep
+            // 
+            this.txtCep.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCep.Location = new System.Drawing.Point(24, 469);
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(238, 26);
+            this.txtCep.TabIndex = 13;
+            // 
+            // lblCep
+            // 
+            this.lblCep.AutoSize = true;
+            this.lblCep.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCep.Location = new System.Drawing.Point(24, 433);
+            this.lblCep.Name = "lblCep";
+            this.lblCep.Size = new System.Drawing.Size(127, 20);
+            this.lblCep.TabIndex = 12;
+            this.lblCep.Text = "Digite o seu cep:";
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelefone.Location = new System.Drawing.Point(29, 527);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(233, 26);
+            this.txtTelefone.TabIndex = 15;
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefone.Location = new System.Drawing.Point(24, 504);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(146, 20);
+            this.lblTelefone.TabIndex = 14;
+            this.lblTelefone.Text = "Digite seu telefone:";
+            // 
+            // txtObservacao
+            // 
+            this.txtObservacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtObservacao.Location = new System.Drawing.Point(24, 598);
+            this.txtObservacao.Multiline = true;
+            this.txtObservacao.Name = "txtObservacao";
+            this.txtObservacao.Size = new System.Drawing.Size(238, 24);
+            this.txtObservacao.TabIndex = 17;
+            // 
+            // lblObservacao
+            // 
+            this.lblObservacao.AutoSize = true;
+            this.lblObservacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblObservacao.Location = new System.Drawing.Point(24, 566);
+            this.lblObservacao.Name = "lblObservacao";
+            this.lblObservacao.Size = new System.Drawing.Size(183, 20);
+            this.lblObservacao.TabIndex = 16;
+            this.lblObservacao.Text = "Digite a sua observação:";
+            // 
+            // lblDataCadastro
+            // 
+            this.lblDataCadastro.AutoSize = true;
+            this.lblDataCadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDataCadastro.Location = new System.Drawing.Point(24, 637);
+            this.lblDataCadastro.Name = "lblDataCadastro";
+            this.lblDataCadastro.Size = new System.Drawing.Size(157, 20);
+            this.lblDataCadastro.TabIndex = 18;
+            this.lblDataCadastro.Text = "Selecione uma Data:";
+            // 
+            // dtpDataCadastro
+            // 
+            this.dtpDataCadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpDataCadastro.Location = new System.Drawing.Point(24, 671);
+            this.dtpDataCadastro.Name = "dtpDataCadastro";
+            this.dtpDataCadastro.Size = new System.Drawing.Size(200, 26);
+            this.dtpDataCadastro.TabIndex = 20;
+            // 
+            // lboDados
+            // 
+            this.lboDados.FormattingEnabled = true;
+            this.lboDados.Location = new System.Drawing.Point(268, 12);
+            this.lboDados.Name = "lboDados";
+            this.lboDados.Size = new System.Drawing.Size(292, 706);
+            this.lboDados.TabIndex = 21;
+            this.lboDados.SelectedValueChanged += new System.EventHandler(this.lboDados_SelectedValueChanged);
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(76, 727);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.TabIndex = 22;
+            this.btnAdicionar.Text = "Adiconar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 771);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.lboDados);
+            this.Controls.Add(this.dtpDataCadastro);
+            this.Controls.Add(this.lblDataCadastro);
+            this.Controls.Add(this.txtObservacao);
+            this.Controls.Add(this.lblObservacao);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.txtCep);
+            this.Controls.Add(this.lblCep);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.lblCidade);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.lblBairro);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.Label lblBairro;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.Label lblCidade;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.TextBox txtCep;
+        private System.Windows.Forms.Label lblCep;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.TextBox txtObservacao;
+        private System.Windows.Forms.Label lblObservacao;
+        private System.Windows.Forms.Label lblDataCadastro;
+        private System.Windows.Forms.DateTimePicker dtpDataCadastro;
+        private System.Windows.Forms.ListBox lboDados;
+        private System.Windows.Forms.Button btnAdicionar;
+    }
+}
+
---
+++ Loja/Form1.cs
@@ -0,0 +1,93 @@
+﻿using Loja.clienteDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Loja.clienteDataSet;
+
+namespace Loja
+{
+    public partial class Form1 : Form
+    {
+
+        private void atualizarLista()
+        {
+            lboDados.Items.Clear();
+            FuncionariosTableAdapter funcionarios = new FuncionariosTableAdapter();
+            var dados = from linha in funcionarios.GetData()
+                        select linha;
+            foreach (var linha in dados)
+            {
+                lboDados.Items.Add(linha);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            atualizarLista();
+        }
+
+      
+        private void lboDados_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (lboDados.SelectedItem == null)
+            {
+                return;
+            }
+            FuncionariosRow item = lboDados.SelectedItem as FuncionariosRow;
+            txtID.Text = item.Codigo.ToString();
+            txtNome.Text = item.Nome;
+            txtEndereco.Text = item.Endereco;   
+            txtBairro.Text = item.Bairro;   
+            txtCidade.Text = item.Cidade;
+            txtEstado.Text = item.Estado;
+            txtCep.Text = item.CEP;
+            txtTelefone.Text = item.Telefone.ToString("(##) #####-####");
+            txtObservacao.Text = item.Observacao;
+            dtpDataCadastro.Value = item.DataCadastro;
+
+
+
+        }
+        private void LimparTela() 
+        {
+            txtNome.Clear();
+            txtBairro.Clear();
+            txtCidade.Clear();
+            txtEstado.Clear();
+            txtObservacao.Clear();
+            txtTelefone.Clear();
+            txtCep.Clear();
+            txtEndereco.Clear();
+            dtpDataCadastro.Value = DateTime.Now;
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text == null || txtCidade.Text == "" || txtCidade.Text == ""
+                || txtCep.Text == "" || txtEndereco.Text == "" || txtTelefone.Text == "" || txtEstado.Text == "" || dtpDataCadastro.Value == null) return;
+
+            FuncionariosTableAdapter novosDados = new FuncionariosTableAdapter();
+            string nome = txtNome.Text;
+            string cidade = txtCidade.Text;
+            string estado = txtEstado.Text;
+            string cep = txtCep.Text;
+            string endereco = txtEndereco.Text;
+            string bairro = txtBairro.Text;
+            int telefone = int.Parse(txtTelefone.Text);
+            DateTime cadastro = dtpDataCadastro.Value;
+
+            novosDados.Insert(nome, endereco, bairro, cidade, estado, cep, telefone, "" ,cadastro);
+            atualizarLista();
+            LimparTela();
+
+
+        }
+    }
+}
